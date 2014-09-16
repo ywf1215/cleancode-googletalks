5e13ddd6e63571d9c5ad0c0747afbf32b417f8a0
Wenhui Yang <wenhui.yang@daokin.com>
2014-09-16 10:30:23 +0800

    first commit

---
+++ cleancode/src/main/java/com/daokin/cleancode/App.java
@@ -0,0 +1,13 @@
+package com.daokin.cleancode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
